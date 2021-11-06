4ab520bb928f02cb0a7ee66e963220822d8a39c4
rjadyshkom <sergey@rjadysh.com>
2021-11-06 10:15:05 +0300

    fix: Seo component

--- src/helpers/Seo.js
+++ src/helpers/Seo.js
@@ -17,10 +17,6 @@ function Seo({
                 title={title}
                 meta={[
                     {
-                        name: 'viewport',
-                        content: 'width=device-width, initial-scale=1'
-                    },
-                    {
                         name: 'description',
                         content: description
                     },
