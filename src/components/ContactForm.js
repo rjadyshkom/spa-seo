bf69985e927b59a66cc704a1a90e5b7e79ba650b
rjadyshkom <sergey@rjadysh.com>
2021-12-20 22:57:35 +0300

    feat: validation strategies

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,6 @@
-import {useState} from "react";
-import Form from "react-formal";
-import {object, string} from "yup";
+import {useState} from 'react';
+import Form, {ValidateStrategies} from 'react-formal';
+import {object, string} from 'yup';
 
 function ContactForm({handler, isLoading, isSent}) {
     const [formState, setFormState] = useState({})
@@ -15,34 +15,55 @@ function ContactForm({handler, isLoading, isSent}) {
         handler(e, formState)
     }
 
-    function onSubmit(formValue) {
-        handleFormSubmit(formValue);
+    function onFormSubmit(formData) {
+        handleFormSubmit(formData);
     }
 
-
-    const modelSchema = object({
+    const validationSchema = object({
         name: string()
             .min(2, 'Минимум 2 символа')
             .required('Укажите имя'),
         email: string()
-            .min(7, 'Минимум 7 символов')
-            .email('Почта должна содержать символ @ и домен')
-            .required('Укажите почту')
+            .min(2, 'Минимум 7 символов')
+            .matches(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Email должен содержать символ @, домен и доменную зону')
+            .required('Укажите почту'),
+        message: string()
+            .max(100, 'Ограничение в 100 символов. Сформулируешь покороче?')
+            .required('Напишите хоть что-нибудь')
     });
 
+    const {Change, Blur, BlurThenChangeAndBlur} = ValidateStrategies;
+
     return (
-        <Form schema={modelSchema}>
+        <Form schema={validationSchema}>
 
-                <Form.Field name="name" placeholder="Имя" onChange={(e) => handleFieldChange("visitor-name", e)}/>
-                <Form.Message
-                    for={["name"]}
-                    className="validation-error"
-                />
+            <Form.Field name="name"
+                        placeholder="Имя"
+                        onChange={(e) => handleFieldChange("visitor-name", e)}
+                        validateOn={Blur}
+            />
+            <Form.Message
+                for="name"
+                className="validation-error"
+            />
 
-            <Form.Field name="email" placeholder="Почта" onChange={(e) => handleFieldChange("visitor-email", e)}/>
+            <Form.Field name="email"
+                        placeholder="Почта"
+                        onChange={(e) => handleFieldChange("visitor-email", e)}
+                        validateOn={BlurThenChangeAndBlur}/>
+
             <Form.Message for="email" className="validation-error"/>
 
-            <Form.Submit type="submit" onClick={onSubmit}>{isLoading ? "Отправка..." : "Отправить"}</Form.Submit>
+            <Form.Field as={"textarea"}
+                        name="message"
+                        placeholder="Сообщение"
+                        onChange={(e) => handleFieldChange("visitor-message", e)}
+                        validateOn={Change}
+            />
+            <Form.Message for="message" className="validation-error"/>
+
+            <Form.Submit type="submit" onClick={onFormSubmit}>Отправить</Form.Submit>
+            <span>{isLoading ? "Отправляется..." : null}</span>
             <span>{isSent ? "Письмо успешно оправлено" : null}</span>
         </Form>
     )
