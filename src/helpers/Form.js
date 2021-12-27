0d25453303dc9504c8a3b623d1b304d5d67bbba3
rjadyshkom <sergey@rjadysh.com>
2021-12-27 19:00:14 +0300

    fix: sending with error logic

--- src/helpers/Form.js
+++ src/helpers/Form.js
@@ -63,6 +63,7 @@ const Form = (props) => {
 
                         } catch (error) {
                             setState(props.messageError);
+                            setSubmitting(false);
                         }
                     };
 
